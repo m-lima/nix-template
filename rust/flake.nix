60e7ed7b9c95c79d5cca70dd2cb3a3d281c9b06f
Marcelo Lima <m-lima@users.noreply.github.com>
2025-02-13 02:33:28 +0100

    Add cargo-all

--- rust/flake.nix
+++ rust/flake.nix
@@ -96,7 +96,50 @@
 
         devShells.default = craneLib.devShell {
           checks = self.checks.${system};
-          packages = with pkgs; [ cargo-hack ];
+          packages = with pkgs; [
+            cargo-hack
+            (pkgs.writeShellScriptBin "cargo-all" ''
+              #!/usr/bin/env bash
+              shift
+
+              while (( $# > 0 )); do
+                case "$1" in
+                  nightly)
+                    nightly='+nightly' ;;
+                  run|r)
+                    run=1 ;;
+                  clean|c)
+                    clean=1 ;;
+                esac
+                shift
+              done
+
+              if [ $clean ]; then
+                echo "[34mCleaning[m" && \
+                cargo clean
+              fi && \
+              echo "[34mFormatting[m" && \
+              cargo $nightly fmt --all && \
+              echo "[34mChecking main[m" && \
+              cargo $nightly hack --feature-powerset check --workspace $@ && \
+              echo "[34mChecking examples[m" && \
+              cargo $nightly hack --feature-powerset check --workspace --examples $@ && \
+              echo "[34mChecking tests[m" && \
+              cargo $nightly hack --feature-powerset check --workspace --tests $@ && \
+              echo "[34mLinting main[m" && \
+              cargo $nightly hack --feature-powerset clippy --workspace $@ && \
+              echo "[34mLinting tests[m" && \
+              cargo $nightly hack --feature-powerset clippy --workspace --tests $@ && \
+              echo "[34mLinting examples[m" && \
+              cargo $nightly hack --feature-powerset clippy --workspace --examples $@ && \
+              echo "[34mTesting main[m" && \
+              cargo $nightly hack --feature-powerset test --workspace $@ && \
+              if [ "$run" ]; then
+                echo "[34mRunning[m" && \
+                cargo $nightly run $@
+              fi
+            '')
+          ];
         };
 
         formatter =
