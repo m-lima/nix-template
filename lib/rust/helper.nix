40b0fd9ca0fb8f4f56f10bcfb0c76dec1f059317
Marcelo Lima <m-lima@users.noreply.github.com>
2025-06-13 20:19:57 +0200

    Allow cargo all as part of checks

--- lib/rust/helper.nix
+++ lib/rust/helper.nix
@@ -13,6 +13,7 @@
   lockRandomSeed ? false,
   mega ? true,
   binary ? true,
+  hack ? false,
   fmts ? [ ],
   buildInputs ? pkgs: [ ],
   nativeBuildInputs ? pkgs: [ ],
@@ -122,50 +123,115 @@ flake-utils.lib.eachDefaultSystem (
         };
       }).config.build;
 
+    cargoAll = pkgs.writeShellScriptBin "cargo-all" ''
+      shift
+
+      skip="--skip default"
+
+      while (( $# > 0 )); do
+        case "$1" in
+          nightly)
+            nightly='+nightly' ;;
+          run|r)
+            run=1 ;;
+          clean|c)
+            clean=1 ;;
+          skip|s)
+            shift
+            skip="--skip $1"
+            ;;
+        esac
+        shift
+      done
+
+      if [ $clean ]; then
+        echo "[34mCleaning[m" && \
+        cargo clean
+      fi && \
+      echo "[34mFormatting[m" && \
+      cargo $nightly fmt --all && \
+      echo "[34mChecking main[m" && \
+      cargo $nightly hack --feature-powerset $skip check --workspace $@ && \
+      echo "[34mChecking examples[m" && \
+      cargo $nightly hack --feature-powerset $skip check --workspace --examples $@ && \
+      echo "[34mChecking tests[m" && \
+      cargo $nightly hack --feature-powerset $skip check --workspace --tests $@ && \
+      echo "[34mLinting main[m" && \
+      cargo $nightly hack --feature-powerset $skip clippy --workspace $@ && \
+      echo "[34mLinting tests[m" && \
+      cargo $nightly hack --feature-powerset $skip clippy --workspace --tests $@ && \
+      echo "[34mLinting examples[m" && \
+      cargo $nightly hack --feature-powerset $skip clippy --workspace --examples $@ && \
+      echo "[34mTesting main[m" && \
+      cargo $nightly hack --feature-powerset $skip test --workspace $@ && \
+      if [ "$run" ]; then
+        echo "[34mRunning[m" && \
+        cargo $nightly run $@
+      fi
+    '';
+
     checkSetup =
       {
         formatting = treefmt.check self;
-
-        clippy = craneLib.cargoClippy (
-          commonArgs
-          // {
-            inherit cargoArtifacts;
-            cargoClippyExtraArgs = "-- -D warnings -W clippy::pedantic";
-          }
-        );
-
-        clippy-tests = craneLib.cargoClippy (
-          commonArgs
-          // {
-            inherit cargoArtifacts;
-            pnameSuffix = "-clippy-tests";
-            cargoClippyExtraArgs = "--tests -- -D warnings -W clippy::pedantic";
-          }
-        );
-
-        clippy-examples = craneLib.cargoClippy (
-          commonArgs
-          // {
-            inherit cargoArtifacts;
-            pnameSuffix = "-clippy-examples";
-            cargoClippyExtraArgs = "--examples -- -D warnings -W clippy::pedantic";
-          }
-        );
-
-        docs = craneLib.cargoDoc (
-          commonArgs
-          // {
-            inherit cargoArtifacts;
-          }
-        );
-
-        tests = craneLib.cargoTest (
-          commonArgs
-          // {
-            inherit cargoArtifacts;
-          }
-        );
       }
+      // (
+        if hack then
+          {
+            hack = craneLib.mkCargoDerivation (
+              commonArgs
+              // {
+                inherit cargoArtifacts;
+                buildPhaseCargoCommand = "cargo all";
+                nativeBuildInputs = (commonArgs.nativeBuildInputs or [ ]) ++ [
+                  pkgs.cargo-hack
+                  cargoAll
+                ];
+              }
+            );
+          }
+        else
+          {
+            clippy = craneLib.cargoClippy (
+              commonArgs
+              // {
+                inherit cargoArtifacts;
+                cargoClippyExtraArgs = "-- -D warnings -W clippy::pedantic";
+              }
+            );
+
+            clippy-tests = craneLib.cargoClippy (
+              commonArgs
+              // {
+                inherit cargoArtifacts;
+                pnameSuffix = "-clippy-tests";
+                cargoClippyExtraArgs = "--tests -- -D warnings -W clippy::pedantic";
+              }
+            );
+
+            clippy-examples = craneLib.cargoClippy (
+              commonArgs
+              // {
+                inherit cargoArtifacts;
+                pnameSuffix = "-clippy-examples";
+                cargoClippyExtraArgs = "--examples -- -D warnings -W clippy::pedantic";
+              }
+            );
+
+            docs = craneLib.cargoDoc (
+              commonArgs
+              // {
+                inherit cargoArtifacts;
+              }
+            );
+
+            tests = craneLib.cargoTest (
+              commonArgs
+              // {
+                inherit cargoArtifacts;
+              }
+            );
+          }
+      )
       // (lib.optionalAttrs (builtins.hasAttr "readme" checks && checks.readme) {
         readme = craneLib.mkCargoDerivation (
           commonArgs
@@ -216,50 +282,7 @@ flake-utils.lib.eachDefaultSystem (
 
       packages = with pkgs; [
         cargo-hack
-        (pkgs.writeShellScriptBin "cargo-all" ''
-          shift
-
-          while (( $# > 0 )); do
-            case "$1" in
-              nightly)
-                nightly='+nightly' ;;
-              run|r)
-                run=1 ;;
-              clean|c)
-                clean=1 ;;
-              skip|s)
-                shift
-                skip="--skip $1"
-                ;;
-            esac
-            shift
-          done
-
-          if [ $clean ]; then
-            echo "[34mCleaning[m" && \
-            cargo clean
-          fi && \
-          echo "[34mFormatting[m" && \
-          cargo $nightly fmt --all && \
-          echo "[34mChecking main[m" && \
-          cargo $nightly hack --feature-powerset $skip check --workspace $@ && \
-          echo "[34mChecking examples[m" && \
-          cargo $nightly hack --feature-powerset $skip check --workspace --examples $@ && \
-          echo "[34mChecking tests[m" && \
-          cargo $nightly hack --feature-powerset $skip check --workspace --tests $@ && \
-          echo "[34mLinting main[m" && \
-          cargo $nightly hack --feature-powerset $skip clippy --workspace $@ && \
-          echo "[34mLinting tests[m" && \
-          cargo $nightly hack --feature-powerset $skip clippy --workspace --tests $@ && \
-          echo "[34mLinting examples[m" && \
-          cargo $nightly hack --feature-powerset $skip clippy --workspace --examples $@ && \
-          echo "[34mTesting main[m" && \
-          cargo $nightly hack --feature-powerset $skip test --workspace $@ && \
-          if [ "$run" ]; then
-            echo "[34mRunning[m" && \
-            cargo $nightly run $@
-          fi
-        '')
+        cargoAll
       ];
     };
   }
