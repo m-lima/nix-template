52bce399b18fdd84768b32c5907781b3ed453047
Marcelo Lima <m-lima@users.noreply.github.com>
2025-07-14 19:12:10 +0200

    Add polars

---
+++ template/polars/flake.nix
@@ -0,0 +1,31 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs =
+    {
+      nixpkgs,
+      flake-utils,
+      ...
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          shellHook = ''
+            nu -c "plugin add ${pkgs.nushellPlugins.polars}/bin/nu_plugin_polars"
+            nu -c "plugin use ${pkgs.nushellPlugins.polars}/bin/nu_plugin_polars"
+          '';
+          buildInputs = [
+            pkgs.nushell
+            pkgs.nushellPlugins.polars
+          ];
+        };
+      }
+    );
+}
