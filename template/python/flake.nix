7b66b6e8fd7a5eae63ecf5e2c33b45be44f62195
Marcelo Lima <m-lima@users.noreply.github.com>
2025-09-23 14:02:06 +0200

    Fix nixpkgs version

--- template/python/flake.nix
+++ template/python/flake.nix
@@ -1,6 +1,6 @@
 {
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
     flake-utils.url = "github:numtide/flake-utils";
     treefmt-nix = {
       url = "github:numtide/treefmt-nix";
