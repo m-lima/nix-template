7170ba99eca79c5a64bb158f2532e4a04d9cd7c9
Marcelo Lima <m-lima@users.noreply.github.com>
2025-10-19 14:17:31 +0200

    Make python easier to use

--- lib/python/helper.nix
+++ lib/python/helper.nix
@@ -7,12 +7,20 @@
 {
   fmts ? [ ],
   buildInputs ? _: [ ],
+  pythonPkgs ? _: [ ],
 }:
 flake-utils.lib.eachDefaultSystem (
   system:
   let
     pkgs = nixpkgs.legacyPackages.${system};
     inherit (pkgs) lib;
+    python = pkgs.python3.withPackages (
+      ps:
+      (pythonPkgs ps)
+      ++ [
+        ps.venvShellHook
+      ]
+    );
     treefmt =
       (treefmt-nix.lib.evalModule pkgs {
         projectRootFile = "flake.nix";
@@ -44,10 +52,24 @@ flake-utils.lib.eachDefaultSystem (
   {
     formatter = treefmt.wrapper;
     devShells.default = pkgs.mkShell {
-      buildInputs = [
-        pkgs.python312
-      ]
-      ++ buildInputs pkgs;
+      packages = [ python ];
+      buildInputs = buildInputs pkgs;
+
+      shellHook = ''
+        SOURCE_DATE_EPOCH=$(date +%s)
+        VENV=.venv
+
+        if test ! -d $VENV; then
+          python3 -m venv $VENV
+        fi
+        source ./$VENV/bin/activate
+        export PYTHONPATH=$(pwd)/$VENV/${python.sitePackages}/:$PYTHONPATH
+        pip install -r requirements.txt
+      '';
+
+      postShellHook = ''
+        ln -sf ${python.sitePackages}/* ./.venv/${python.sitePackages}
+      '';
     };
   }
 )
