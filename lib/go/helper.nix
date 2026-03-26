ecc41d727eec5fc0e5f8c73db64fc8b1e91db336
Marcelo Lima <m-lima@users.noreply.github.com>
2026-03-27 00:53:27 +0100

    Unwrap go systems

--- lib/go/helper.nix
+++ lib/go/helper.nix
@@ -1,12 +1,11 @@
 {
   self,
   nixpkgs,
-  flake-utils,
   treefmt-nix,
   gomod2nix,
   ...
 }:
-root:
+system: root:
 {
   pname,
   version,
@@ -26,71 +25,68 @@ root:
   # General
   overrides ? { },
 }:
-flake-utils.lib.eachDefaultSystem (
-  system:
-  let
-    pkgs = nixpkgs.legacyPackages.${system};
-    util = import ../util.nix;
-    inherit (util) override;
-    tryOverride = util.tryOverride overrides;
+let
+  pkgs = nixpkgs.legacyPackages.${system};
+  util = import ../util.nix;
+  inherit (util) override;
+  tryOverride = util.tryOverride overrides;
 
-    go2nix = gomod2nix.legacyPackages.${system};
+  go2nix = gomod2nix.legacyPackages.${system};
 
-    fmtConfig = tryOverride "treefmt" {
-      projectRootFile = "flake.nix";
-      programs = override formatters {
-        nixfmt.enable = true;
-        gofumpt.enable = true;
-        goimports.enable = true;
-        yamlfmt.enable = true;
-      };
-      settings = override fmtSettings (util.fmtSettings [ "gomod2nix.toml" ] fmtExcludes);
+  fmtConfig = tryOverride "treefmt" {
+    projectRootFile = "flake.nix";
+    programs = override formatters {
+      nixfmt.enable = true;
+      gofumpt.enable = true;
+      goimports.enable = true;
+      yamlfmt.enable = true;
     };
+    settings = override fmtSettings (util.fmtSettings [ "gomod2nix.toml" ] fmtExcludes);
+  };
 
-    treefmt = (treefmt-nix.lib.evalModule pkgs fmtConfig).config.build;
-  in
-  {
-    packages.default = tryOverride "package" (
-      go2nix.buildGoApplication (
-        tryOverride "packageArgs" {
-          inherit
-            pname
-            version
-            src
-            pwd
-            ;
+  treefmt = (treefmt-nix.lib.evalModule pkgs fmtConfig).config.build;
+in
+{
+  packages.default = tryOverride "package" (
+    go2nix.buildGoApplication (
+      tryOverride "packageArgs" {
+        inherit
+          pname
+          version
+          src
+          pwd
+          ;
 
-          buildInputs = buildInputs pkgs;
-          nativeBuildInputs = nativeBuildInputs pkgs;
-        }
-      )
-    );
-    checks = {
-      formatting = treefmt.check self;
-      lint = go2nix.buildGoApplication {
-        inherit src pwd;
-        name = "lint";
-        dontBuild = true;
-        doCheck = true;
-        nativeBuildInputs = [
-          pkgs.golangci-lint
-          pkgs.writableTmpDirAsHomeHook
-        ];
-        checkPhase = "golangci-lint run";
-        installPhase = "mkdir $out";
-      };
-    };
-    formatter = treefmt.wrapper;
-    devShells.default = pkgs.mkShell {
-      packages = devPackages pkgs;
-      buildInputs = [
-        go2nix.gomod2nix
-        pkgs.go
-        pkgs.gopls
-        pkgs.gofumpt
+        buildInputs = buildInputs pkgs;
+        nativeBuildInputs = nativeBuildInputs pkgs;
+      }
+    )
+  );
+  checks = {
+    formatting = treefmt.check self;
+    lint = go2nix.buildGoApplication {
+      inherit src pwd;
+      name = "lint";
+      dontBuild = true;
+      doCheck = true;
+      nativeBuildInputs = [
         pkgs.golangci-lint
-        pkgs.golangci-lint-langserver
+        pkgs.writableTmpDirAsHomeHook
       ];
+      checkPhase = "golangci-lint run";
+      installPhase = "mkdir $out";
     };
-  }
-)
+  };
+  formatter = treefmt.wrapper;
+  devShells.default = pkgs.mkShell {
+    packages = devPackages pkgs;
+    buildInputs = [
+      go2nix.gomod2nix
+      pkgs.go
+      pkgs.gopls
+      pkgs.gofumpt
+      pkgs.golangci-lint
+      pkgs.golangci-lint-langserver
+    ];
+  };
+}
